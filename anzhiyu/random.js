1a91a3b15764adcf46a1a5f0f361c1e7c766daa4
qihua827 <qihua@qq.com>
2023-12-23 09:28:04 +0800

    Site updated: 2023-12-23 09:28:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/01/hello-world/","2023/12/07/JUM并发编程/"];function toRandomPost(){
+var posts=["2023/12/07/JUM并发编程/","2023/12/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
